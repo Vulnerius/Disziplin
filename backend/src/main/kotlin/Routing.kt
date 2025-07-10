8c7ec5eb864099bde2445ada56e889f394d39afe
Paul Fuchs <64081175+Vulnerius@users.noreply.github.com>
2025-07-10 16:38:21 +0200

    add pi dns host

--- backend/src/main/kotlin/Routing.kt
+++ backend/src/main/kotlin/Routing.kt
@@ -26,6 +26,7 @@ fun Application.configureRouting() {
 
     install(CORS) {
         allowHost("192.168.2.111:30613", schemes = listOf("http")) // TODO check here
+        allowHost("eevee:30613", schemes = listOf("http"))
         allowMethod(HttpMethod.Get)
         allowMethod(HttpMethod.Post)
         allowMethod(HttpMethod.Put)
